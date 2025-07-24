9b8c782604e9c24ad20fd5154e31c9d72d316a67
CodingIran <codingiran@gmail.com>
2025-07-24 15:30:28 +0800

    Release 0.0.8

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/codingiran/AsyncTimer.git", from: "0.0.4"),
-        .package(url: "https://github.com/codingiran/NetworkPathMonitor.git", from: "0.1.0"),
+        .package(url: "https://github.com/codingiran/NetworkPathMonitor.git", from: "0.1.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
