79dcd25666e074a82341f2c8745e4c17ebb1ff24
CodingIran <codingiran@gmail.com>
2025-08-08 12:03:56 +0800

    update dependency

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/codingiran/AsyncTimer.git", from: "0.0.6"),
+        .package(url: "https://github.com/codingiran/AsyncTimer.git", from: "0.0.7"),
         .package(url: "https://github.com/codingiran/NetworkPathMonitor.git", from: "0.1.5"),
     ],
     targets: [
